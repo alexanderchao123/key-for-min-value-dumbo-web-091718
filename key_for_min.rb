928c1f92daec5b0eaff57963ab0d30a7bc373646
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-10 06:09:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each {|name, value| max = value if value > max}
 
   key = ""
+  min = max
   name_hash.each do |name, value|
     min = value if value < min
     key = name
